a1ad366967e5c6d20389c99ccdaa289a3cf9a6fa
ronaldoaf <ronaldoaf>
2016-06-07 12:01:46 -0300

    adiciona a variável com o código do 'Cardeno Excluir'

--- qconcursos_botao_excluir.user.js
+++ qconcursos_botao_excluir.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         QConcursos_botao_excluir
+// @name         qconcursos_botao_excluir
 // @namespace    https://github.com/ronaldoaf/tampermonkey
 // @version      0.1
 // @description  Adiciona o botão excluir, que na verdade coloca a questão em uma caderno específico
@@ -11,6 +11,8 @@
 (function() {
     'use strict';
 
+var ID_CARNERNO_EXCLUIR=1432447;
+
 //Cria adiciona o botão excluir logo abaixo do responder
 $('.ipt-responder').each(function(i,e){
         $(e).html( $(e).html()+'<span style="background-color:yellow;font-size:20px">EXCLUIR</span>');
@@ -21,7 +23,7 @@ $('.ipt-responder span').click(function(){
         var id_questao=$(this).parent().attr('id').split('-')[1];
 
         $.ajax({
-            url: 'https://www.qconcursos.com/cadernos/adicionar/questoes/'+id_questao+'/1432447',
+            url: 'https://www.qconcursos.com/cadernos/adicionar/questoes/'+id_questao+'/'+ID_CARNERNO_EXCLUIR,
             headers: {
                 'X-Requested-With':'XMLHttpRequest'
             },
